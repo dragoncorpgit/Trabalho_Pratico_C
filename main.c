5bf269da04fcc5aa3d69afa5470d20b3dd51344f
dragoncorpgit <ricagamer@hotmail.com>
2014-06-11 10:30:49 +0100

    1

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: gaming2
+ *
+ * Created on June 11, 2014, 10:29 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
