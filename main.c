697b682393de8e74fea9fb540161d4bd3d80adad
root <root@unknown-pc>
2014-06-16 09:59:00 +0100

    1

--- main.c
+++ main.c
@@ -2,14 +2,7 @@
 #include <stdlib.h>
 #include "date.h"
 
-typedef enum {
-    CARS = 0,
-    SIMPLE_MOTORCYLES = 1,
-    HIGH_CYLINDER_MOTORCYLES  = 2,
-    TRUCKS = 3,
-    BOATS_MOTOR = 4,
-    BOATS_BI_MOTOR = 5
-} enumSpeciality;
+
 
 int main(void) 
 {
@@ -18,14 +11,7 @@ int main(void)
 }
 
 
-bool IsSpecialityValid (unsigned short int inputSpeciality) {
-    if (inputSpeciality < 0 || inputSpeciality > 5){
-        printf("Especialidade inválido, insira corretamente");
-        return false;
-    }else {
-        return true;
-    }
-}
+
 
 
 
--- speciality.c
+++ speciality.c
@@ -1,1 +1,17 @@
+typedef enum {
+    CARS = 0,
+    SIMPLE_MOTORCYLES = 1,
+    HIGH_CYLINDER_MOTORCYLES  = 2,
+    TRUCKS = 3,
+    BOATS_MOTOR = 4,
+    BOATS_BI_MOTOR = 5
+} enumSpeciality;
 
+bool IsSpecialityValid (unsigned short int inputSpeciality) {
+    if (inputSpeciality < 0 || inputSpeciality > 5){
+        printf("Especialidade inválido, insira corretamente");
+        return false;
+    }else {
+        return true;
+    }
+}a
